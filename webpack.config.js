0fcf217e0ee50e7edb6aac7bc52e78f667005513
thoffmann <toni.hoffmann@arthrex.de>
2017-11-13 10:04:31 +0100

    including uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 /* globals module, __dirname */
 'use strict';
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
     entry: "./src/jquery.xgallerify.js",
     output: {
-        path: __dirname,
-        filename: "./dist/jquery.xgallerify.js"
+        path: path.resolve(__dirname, 'dist'),
+        filename: "jquery.xgallerify.js"
     },
     module: {
         loaders: [
             // { test: /\.css$/, loader: "style!css" }
         ]
     },
-    // plugins: [
-    //     new webpack.optimize.UglifyJsPlugin({
-    //       include: /\.min\.js$/,
-    //       minimize: true
-    //     })
-    // ]
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+          minimize: true
+        })
+    ]
 };
